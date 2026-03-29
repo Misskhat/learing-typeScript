e77bd17de4a92d3f45dc0f398c1c4dab04f6829a
Misskhat <misskhat.hossen@gmail.com>
2026-03-29 19:13:50 +0600

    class and constructor declaration

--- 4th-class/script.js
+++ 4th-class/script.js
@@ -21,4 +21,23 @@ const d1 = new Devise();
 const d2 = new Devise();
 console.log(d1);
 console.log(d2);
+// In simple is a diagram where you define default value and if you need the dynamic value the need to use constructor. Example given below:
+class BottleMaker {
+    name;
+    material;
+    price;
+    brandIcon;
+    constructor(name, material, price, brandIcon = "brandicon.png") {
+        this.name = name;
+        this.material = material;
+        this.price = price;
+        this.brandIcon = brandIcon;
+        this.brandIcon = brandIcon;
+    }
+    isLabeling = true;
+}
+const b1 = new BottleMaker("MUM", "Metal", 1000, "mumIcon.png");
+const b2 = new BottleMaker("RFL", "Plastic", 120);
+console.log(b1);
+console.log(b2);
 //# sourceMappingURL=script.js.map
--- 4th-class/script.ts
+++ 4th-class/script.ts
@@ -23,3 +23,23 @@ const d2 = new Devise();
 
 console.log(d1);
 console.log(d2);
+
+// In simple is a diagram where you define default value and if you need the dynamic value the need to use constructor. Example given below:
+
+class BottleMaker {
+  constructor(
+    public name: string,
+    public material: string,
+    public price: number,
+    public brandIcon: string = "brandicon.png",
+  ) {
+    this.brandIcon = brandIcon;
+  }
+  isLabeling = true;
+}
+
+const b1 = new BottleMaker("MUM", "Metal", 1000, "mumIcon.png");
+const b2 = new BottleMaker("RFL", "Plastic", 120);
+
+console.log(b1);
+console.log(b2);
