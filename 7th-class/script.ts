9ab31682bf47ecc8947bc170a2490e63f9cd1eda
Misskhat <misskhat.hossen@gmail.com>
2026-04-03 16:54:22 +0600

    modules export and import details added

---
+++ 7th-class/script.ts
@@ -0,0 +1,16 @@
+/**
+ * Modules
+ * Exporting and Importing modules
+ * Defaults Export
+ *
+ *  */
+
+/**
+ * In simple way modules is export and import from one file or folder to another file.
+ * Two (2) way we may export items
+ * 1. Basic export, just use "export" key word in front of items.
+ * 2. Export default, it is only export one item from a file.
+ * */
+
+// Example of export: export function abcd(a:number){console.log(a)}
+// Example of import: import {abcd} from ./filename
